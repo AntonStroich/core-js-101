cb891a8fc94125f98403828f7ccf481e882c91d8
Anton.Stroich <anton.stroich@ventionteams.com>
2025-02-12 11:45:27 +0400

    conditions 5

--- src/07-conditions-n-loops-tasks.js
+++ src/07-conditions-n-loops-tasks.js
@@ -137,8 +137,23 @@ function isTriangle(a, b, c) {
  *   { top:20, left:20, width: 20, height: 20 }    =>  false
  *
  */
-function doRectanglesOverlap(/* rect1, rect2 */) {
-  throw new Error('Not implemented');
+function doRectanglesOverlap(rect1, rect2) {
+// The intersection width is calculated by finding the overlap along the x-axis
+// (rightmost left edge minus the leftmost right edge). If there's no overlap,
+// the result will be 0 or negative, so we return the max value between 0 and the result.
+// The intersection height is calculated by finding the overlap along the y-axis
+// (bottommost top edge minus the topmost bottom edge). Similarly, if there's no overlap,
+// the result will be 0 or negative, so we return the max value between 0 and the result.
+
+  const condition1 = rect1.left + rect1.width <= rect2.left;
+  const condition2 = rect2.left + rect2.width <= rect1.left;
+  const condition3 = rect1.top + rect1.height <= rect2.top;
+  const condition4 = rect2.top + rect2.height <= rect1.top;
+
+  if (condition1 || condition2 || condition3 || condition4) {
+    return false;
+  }
+  return true;
 }
 
 
