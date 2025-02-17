9870b934497a630a572cb7d53b9a9ce5b43644e3
Anton.Stroich <anton.stroich@ventionteams.com>
2025-02-17 10:45:02 +0400

    Conditions 6

--- src/07-conditions-n-loops-tasks.js
+++ src/07-conditions-n-loops-tasks.js
@@ -183,8 +183,12 @@ function doRectanglesOverlap(rect1, rect2) {
  *   { center: { x:0, y:0 }, radius:10 },  { x:10, y:10 }   => false
  *
  */
-function isInsideCircle(/* circle, point */) {
-  throw new Error('Not implemented');
+function isInsideCircle(circle, point) {
+  const distance = Math.sqrt((point.x - circle.center.x) ** 2 + (point.y - circle.center.y) ** 2);
+  if (distance < circle.radius) {
+    return true; // Point is strictly inside the circle
+  }
+  return false; // Point is either on the boundary or outside the circle
 }
 
 
