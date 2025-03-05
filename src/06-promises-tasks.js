41fc8ed9c73cc2492186eba33f22e4c995d00528
Anton.Stroich <anton.stroich@ventionteams.com>
2025-03-05 13:00:32 +0400

    promises 2 - 4

--- src/06-promises-tasks.js
+++ src/06-promises-tasks.js
@@ -57,8 +57,8 @@ function willYouMarryMe(isPositiveAnswer) {
  *    })
  *
  */
-function processAllPromises(/* array */) {
-  throw new Error('Not implemented');
+function processAllPromises(array) {
+  return Promise.all(array);
 }
 
 /**
@@ -80,8 +80,8 @@ function processAllPromises(/* array */) {
  *    })
  *
  */
-function getFastestPromise(/* array */) {
-  throw new Error('Not implemented');
+function getFastestPromise(array) {
+  return Promise.race(array);
 }
 
 /**
@@ -101,8 +101,12 @@ function getFastestPromise(/* array */) {
  *    });
  *
  */
-function chainPromises(/* array, action */) {
-  throw new Error('Not implemented');
+function chainPromises(array, action) {
+  return array.reduce((accumulator, current) => accumulator
+    .then((result) => current
+      .then((value) => (result === undefined ? value : action(result, value))) // Combine values
+      .catch(() => result)), // Ignore rejected promises
+  Promise.resolve()); // Start with an empty resolved promise
 }
 
 module.exports = {
